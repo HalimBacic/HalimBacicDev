d8b0a424859b65b7ef56a4a7cdebc30170d5ff9c
HalimBacic <33162351+HalimBacic@users.noreply.github.com>
2022-07-20 09:39:16 +0200

    Navigation gfx added

--- halimbacic/src/components/Home.js
+++ halimbacic/src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../style/home.css'
+import MainNavigation from './MainNavigation'
+
+
+
 
 function Home() {
   return (
     <div className='maincontainer'>
-        
+        <MainNavigation></MainNavigation>
     </div>
   )
 }
--- halimbacic/src/components/MainNavigation.js
+++ halimbacic/src/components/MainNavigation.js
@@ -3,11 +3,22 @@ import Stack from '@mui/material/Stack'
 import { Button } from '@mui/material'
 import '../style/navigation.css'
 
+const simpleButton = {
+      size : '20%',
+      color: '#FA1C37',
+      fontSize: '2rem',
+      fontFamily: 'Ruda, sans-serif'
+
+}
+
 function MainNavigation() {
   return (
-    <div>
-        <Stack direction="column" spacing={2}>
-            <Button className='simpleButton'></Button>
+    <div className='mainClass'>
+        <Stack direction="column" spacing={3} sx={{mt:15, ml:15}}>
+            <Button sx={simpleButton} className='simpleButtonCss'>About me</Button>
+            <Button sx={simpleButton} className='simpleButtonCss'>Projects</Button>
+            <Button sx={simpleButton} className='simpleButtonCss'>In progress</Button>
+            <Button sx={simpleButton} className='simpleButtonCss'>Contacts</Button>
         </Stack>
     </div>
   )
