e9635b111f43741db2caaf5393c1f422dd911366
bacich <bacic.h@aleacontrol.com>
2022-07-22 00:12:49 +0200

    About added

---
+++ halimbacic/src/components/About.js
@@ -0,0 +1,14 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <Stack direction="column">
+
+        </Stack>
+    </div>
+  )
+}
+
+export default About
