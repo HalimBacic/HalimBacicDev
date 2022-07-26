1ea5e4707d1ed2c27a3913b8fddbb48e5d43eaf1
HalimBacic <33162351+HalimBacic@users.noreply.github.com>
2022-07-26 09:05:53 +0200

    About change

--- halimbacic/src/components/About.js
+++ halimbacic/src/components/About.js
@@ -1,15 +1,17 @@
 import { Box, Stack } from '@mui/material'
 import React from 'react'
 import '../style/about.css'
+import { Progress } from "@nextui-org/react";
 
 function About() {
   return (
     <div className='aboutContainer'>
-              <Box className='leftBox' sx={{width:'30%', height:'90%'}}>
-                HALIM
+              <Box className='leftBox' sx={{width:'27.8%', height:'100%'}}>
+                <Box>One</Box>
+                <Box>Two</Box>
               </Box> 
-              <Box className='rightBox' sx={{width:'60%', height:'90%'}}>
-                BACIC
+              <Box className='rightBox' sx={{width:'68%', height:'100%'}}>
+                
               </Box>                
     </div>
   )
