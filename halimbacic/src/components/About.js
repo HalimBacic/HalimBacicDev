1435ea7f9fe0be6527115d568c87d5e03e38dc23
bacich <bacic.h@aleacontrol.com>
2022-07-29 00:12:08 +0200

    About navigation

--- halimbacic/src/components/About.js
+++ halimbacic/src/components/About.js
@@ -1,18 +1,44 @@
 import { Box, Stack } from '@mui/material'
 import React from 'react'
 import '../style/about.css'
-import { Progress } from "@nextui-org/react";
+import { css, Button, Progress } from "@nextui-org/react";
+
+
+const buttonStyle = {
+  borderColor: '#9b1d20', 
+  margin: 10,
+  color: '#FA1C37', 
+  fontWeight: 800,
+  fontSize: 20,
+  textShadow: '2px 2px black'
+}
+
+const simpleButton = {
+  textShadow: '2px 2px black',
+  color: '#FA1C37',
+  fontSize: '2rem',
+  fontFamily: 'Ruda, sans-serif',
+  '&:hover':{
+    textShadow: '1px 1px white, 2px 2px white'
+  }
+}
 
 function About() {
   return (
     <div className='aboutContainer'>
-              <Box className='leftBox' sx={{width:'27.8%', height:'100%'}}>
-                <Box>One</Box>
-                <Box>Two</Box>
-              </Box> 
-              <Box className='rightBox' sx={{width:'68%', height:'100%'}}>
-                
-              </Box>                
+      <div className='leftSide'>
+      </div>
+      <div className='rightSide'>
+        <Button light
+          css={buttonStyle}>HOME</Button>
+        <Stack direction="column" spacing={7}>
+          <Button light css={simpleButton}>About me</Button>
+          <Button light css={simpleButton}>Education</Button>
+          <Button light css={simpleButton}>Experience</Button>
+        </Stack>
+        <Button light
+          css={buttonStyle}>PROJECTS</Button>
+      </div>
     </div>
   )
 }
