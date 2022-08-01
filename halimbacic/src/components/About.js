1c1a2438b4dc34750ee8b2431007faa8f12ad712
bacich <bacic.h@aleacontrol.com>
2022-08-01 21:03:50 +0200

    Some navigation changes

--- halimbacic/src/components/About.js
+++ halimbacic/src/components/About.js
@@ -2,82 +2,86 @@ import { Box, Stack } from '@mui/material'
 import React from 'react'
 import '../style/about.css'
 import { css, Button, Progress } from "@nextui-org/react";
-import {MdOutlineArrowDropUp} from 'react-icons/md'
-import {MdOutlineArrowDropDown} from 'react-icons/md'
+import { MdOutlineArrowDropUp } from 'react-icons/md'
+import { MdOutlineArrowDropDown } from 'react-icons/md'
+import { FaLaptopCode } from 'react-icons/fa'
+import { AiFillHome } from 'react-icons/ai'
+import { useState } from 'react'
 
 
 
 const buttonStyleUp = {
-  borderColor: '#9b1d20', 
+  borderColor: '#9b1d20',
   margin: 15,
-  marginLeft: '40%',
-  color: '#FA1C37', 
+  color: '#FA1C37',
   fontWeight: 700,
   fontSize: 16,
-  width:1,
   textShadow: '1px 1px black',
-  '&:hover':{
-    textShadow: '1px 1px white, 2px 2px white'
+  '&:hover': {
+    textShadow: '1px 1px white, 2px 2px white',
+    borderBottom: '2px solid #FA1C37',
+    borderRadius:0
   }
 }
 
 const buttonStyleDown = {
-  borderColor: '#9b1d20', 
-  margin: 15,
-  marginLeft: '40%',
-  color: '#FA1C37', 
+  borderColor: '#9b1d20',
+  color: '#FA1C37',
   fontWeight: 700,
   fontSize: 18,
-  width:1,
   textShadow: '1px 1px black',
   borderUp: '3px solid #f4796b',
-  '&:hover':{
-    textShadow: '1px 1px white, 2px 2px white'
+  '&:hover': {
+    textShadow: '1px 1px white, 2px 2px white',
+    borderBottom: '2px solid #FA1C37',
+    borderRadius:0
   }
 }
 
 const simpleButton = {
   textShadow: '2px 2px black',
   color: '#FA1C37',
-  fontSize: '2rem',
+  fontSize: '1.4rem',
+  height: '100%',
+  width:'90%',
   fontFamily: 'Ruda, sans-serif',
-  '&:hover':{
-    textShadow: '1px 1px white, 2px 2px white'
+  '&:hover': {
+    textShadow: '1px 1px white, 2px 2px white',
+    backgroundColor:'rgba(78, 205, 196, 0.3)'
   }
 }
 
-const smallTextButton = {
-  borderColor: '#9b1d20', 
-  margin: 15,
-  marginLeft: '40%',
-  color: '#FA1C37', 
-  fontWeight: 700,
-  fontSize: 15,
-  width:1,
-  textShadow: '1px 1px black',
-  '&:hover':{
-    textShadow: '1px 1px white, 2px 2px white'
-  }
-}
 
 function About() {
+
+  const [intro, setIntro] = useState(false);
+
   return (
     <div className='aboutContainer'>
-      <div className='leftSide'>
-      </div>
-      <div className='rightSide'>
+      <div className='topPage'>
         <Button light
           css={buttonStyleUp}
-          iconRight={<MdOutlineArrowDropUp className="iconCustom"></MdOutlineArrowDropUp>}
-          >HOME</Button>
-        <Stack direction="column" spacing={7}>
+          icon={<AiFillHome className="iconCustom"></AiFillHome>}>
+          Home
+        </Button>
+        <Stack direction="row" spacing={7} sx={{height:'100%'}}>
           <Button light css={simpleButton}>About me</Button>
           <Button light css={simpleButton}>Education</Button>
           <Button light css={simpleButton}>Experience</Button>
         </Stack>
         <Button light
-          iconRight={<MdOutlineArrowDropDown className="iconCustom"></MdOutlineArrowDropDown>}
-          css={smallTextButton}>PROJECTS</Button>
+          iconRight={<FaLaptopCode className="iconCustom"></FaLaptopCode>}
+          css={buttonStyleDown}>
+          Projects
+        </Button>
+      </div>
+      <div className='bottomPage'>
+        {intro &&
+          <Box>
+            I am Halim, software developer from Banja Luka.
+            Let me to
+          </Box>
+        }
       </div>
     </div>
   )
--- halimbacic/src/components/MainNavigation.js
+++ halimbacic/src/components/MainNavigation.js
@@ -1,26 +1,61 @@
 import React from 'react'
+import { useState } from 'react'
 import Stack from '@mui/material/Stack'
-import { Button } from '@mui/material'
+import { Box, Button } from '@mui/material'
+import IconButton from '@mui/material/IconButton';
 import Sign from './Sign'
 import '../style/navigation.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const simpleButton = {
-      size : '20%',
-      color: '#FA1C37',
-      fontSize: '2rem',
-      fontFamily: 'Ruda, sans-serif'
+  size: '20%',
+  color: '#FA1C37',
+  fontSize: '2rem',
+  fontFamily: 'Ruda, sans-serif'
+}
+
+const iconButton = { 
+  color: '#FA1C37',
+  top:10,
+  left:10,
 }
 
 function MainNavigation() {
+
+  const [isVisibleMenu, setisVisibleMenu] = useState(false);
+  const [isVisibleAbout, setisVisibleAbout] = useState(true);
+
+  const menuClick = () => {
+    if (isVisibleAbout === true) {
+      setisVisibleAbout(false);
+      setisVisibleMenu(true);
+    }
+    else {
+      setisVisibleAbout(true);
+      setisVisibleMenu(false);
+    }
+  }
+
   return (
-    <div className='mainClass'>
-        <Stack direction="column" spacing={3} sx={{mt:15, ml:15}}>
+    <div>
+      <Button sx={iconButton} size="large" onClick={menuClick} startIcon={<MenuIcon></MenuIcon>}>
+        Menu
+      </Button>
+      {isVisibleAbout &&
+        <Box id="firstInfoBox">
+
+        </Box>
+      }
+      {isVisibleMenu &&
+        <div className='mainClass'>
+          <Stack direction="column" spacing={3} sx={{ mt: 15, ml: 15 }}>
             <Button sx={simpleButton} className='simpleButtonCss'>About me</Button>
             <Button sx={simpleButton} className='simpleButtonCss'>Projects</Button>
             <Button sx={simpleButton} className='simpleButtonCss'>In progress</Button>
             <Button sx={simpleButton} className='simpleButtonCss'>Contacts</Button>
-            {/* <Sign></Sign> */}
-        </Stack>
+          </Stack>
+        </div>
+      }
     </div>
   )
 }
